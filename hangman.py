a9a1fb3e0d4aeb3163e486fa85fbca0c1dce3993
michaelc445 <michaelc2468@gmail.com>
2019-02-04 17:08:21 +0000

    made some improvements

--- hangman.py
+++ hangman.py
@@ -1,5 +1,4 @@
 import random
-
 import tester
 
 
@@ -8,7 +7,7 @@ list = []
 list4 = []
 list7 = []
 list10 = []
-for word in f.read().split():
+for word in f.read().split(): ## splits 20k.txt into lists of different sized words
     if len(word) == 4:
         list4.append(word)
     elif len(word) == 7:
@@ -22,24 +21,24 @@ diff = 0
 secretWord = ""
 while tries < 10:
     try:  
-        diff = int(input("Please select difficulty level. 1: easy 2: medium 3: hard "))
+        diff = int(input("Please select difficulty level. 1: easy 2: medium 3: hard ")) ## get game mode
         break
     except ValueError:
         print("enter 1 2 or 3")
         tries += 1
-
+## decide which word list to use.
 if diff == 1:
     randNum = random.randint(0, len(list4))
     secretWord = list4[randNum]
-    tester.easy(secretWord)
+    tester.game(secretWord, 1)
 elif diff == 2:
     randNum = random.randint(0, len(list7))
     secretWord = list7[randNum]
-    tester.medium(secretWord)
+    tester.game(secretWord, 2)
 else:
     randNum = random.randint(0, len(list10))
     secretWord = list10[randNum]
-    tester.hard(secretWord)
+    tester.game(secretWord, 3)
 
 
 
--- tester.py
+++ tester.py
@@ -1,27 +1,25 @@
-
+## returns index of item if it is in the word
 def duplicates(lst, item):
     return [i for i, x in enumerate(lst) if x == item]
 
-def easy(tester):
+def game(tester, mode):
+    counter = 7
     temp2 = []
     temp3 = ["_"]*len(tester)
-    for chars in tester:
+    for chars in tester: ## splits string into a list 
         temp2.append(chars)
-    counter = 8
-    temp = "____"
     
     while counter > 0:
         try:
             guess = input("guess a letter. ")
-            if len(guess) > 1:
+            if len(guess) > 1: ## makes sure user only enters 1 letter at a time
                 raise ValueError
-            counter -= 1
             if guess in temp2:
                 ind = duplicates(temp2, guess)
-                for nums in ind:
+                for nums in ind: ##replacing "_" with the correct letter
                     temp3[nums] = guess
 
-                temp4 = ''.join(temp3)
+                temp4 = ''.join(temp3) ## converting list back to a string 
                 print("good guess. %d guesses left " % counter)
                 print(temp4)
                 if temp4 == tester:
@@ -30,96 +28,13 @@ def easy(tester):
 
                 
             else:
+                counter -= 1
                 print("incorrect %d guesses left" % counter)
                 
 
         except ValueError:
             print("1 letter at a time")
-        if counter == 0:
-            print("you lose.")
+        if counter == 0: ## user runs out of guesses
+            print("you lose.") 
             print(tester)
             exit()
-            
-
-    
-    
-
-
-def medium(tester):
-    temp2 = []
-    temp3 = ["_"]*len(tester)
-    for chars in tester:
-        temp2.append(chars)
-    counter = 12
-    temp = "____"
-    
-    while counter > 0:
-        try:
-            guess = input("guess a letter. ")
-            if len(guess) > 1:
-                raise ValueError
-            counter -= 1
-            if guess in temp2:
-                ind = duplicates(temp2, guess)
-                for nums in ind:
-                    temp3[nums] = guess
-
-                temp4 = ''.join(temp3)
-                print("good guess. %d guesses left" % counter)
-                print(temp4)
-                if temp4 == tester:
-                    print("you win!")
-                    exit()
-
-                
-            else:
-                print("incorrect %d guesses left" % counter)
-                
-
-        except ValueError:
-            print("1 letter at a time ")
-        if counter == 0:
-            print("you lose.")
-            print(tester)
-            exit()
-            
-
-
-def hard(tester):
-    temp2 = []
-    temp3 = ["_"]*len(tester)
-    for chars in tester:
-        temp2.append(chars)
-    counter = 15
-    temp = "____"
-    
-    while counter > 0:
-        try:
-            guess = input("guess a letter. ")
-            if len(guess) > 1:
-                raise ValueError
-            counter -= 1
-            if guess in temp2:
-                ind = duplicates(temp2, guess)
-                for nums in ind:
-                    temp3[nums] = guess
-
-                temp4 = ''.join(temp3)
-                print("good guess. %d guesses left" % counter)
-                print(temp4)
-                if temp4 == tester:
-                    print("you win!")
-                    exit()
-
-                
-            else:
-                print("incorrect %d guesses left" % counter)
-                
-
-        except ValueError:
-            print("1 letter at a time ")
-        if counter == 0:
-            print("you lose.")
-            print(tester)
-            exit()
-            
